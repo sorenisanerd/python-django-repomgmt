ce71407f63d82e8117fd8a7dcd4a6b3f6a8d4a7b
Soren Hansen <sorhanse@cisco.com>
2012-11-07 14:14:02 +0100

    Whoops

--- repomgmt/utils.py
+++ repomgmt/utils.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 
 def run_cmd(cmd, input=None):
     logger.info('Executing %r with input=%r' % (cmd, input))
-    if settings.TESING:
+    if settings.TESTING:
         from repomgmt import mock_data
         return mock_data.run_cmd(cmd, input)
     proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
