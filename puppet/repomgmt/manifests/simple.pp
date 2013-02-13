b286eec007487d2908b6d01e636579db2f7c76b6
Soren Hansen <sorhanse@cisco.com>
2013-02-13 09:42:54 +0100

    Fix formatting

--- puppet/repomgmt/manifests/simple.pp
+++ puppet/repomgmt/manifests/simple.pp
@@ -1,4 +1,14 @@
-class repomgmt:simple($user = 'ubuntu',admin_name = "Admin User",admin_email = "email@example.com",$dbname = "repomgmt",$dbuser = "repomgmt",$dbpass = "repomgmtpass",$dbhost = "",$dbport = "",$secret_key = '!tuy9ozxr@zhr$8v3$41^3690dfnrim16yj8x5)4pi0bg%140l',$ftp_ip = $::ipaddress,$post_mk_sbuild_customisation = undef) {
+class repomgmt::simple($user = 'ubuntu',
+                       $admin_name = "Admin User",
+                       $admin_email = "email@example.com",
+                       $dbname = "repomgmt",
+                       $dbuser = "repomgmt",
+                       $dbpass = "repomgmtpass",
+                       $dbhost = "",
+                       $dbport = "",
+                       $secret_key = '!tuy9ozxr@zhr$8v3$41^3690dfnrim16yj8x5)4pi0bg%140l',
+                       $ftp_ip = $::ipaddress,
+                       $post_mk_sbuild_customisation = undef) {
   $simple = true
 
   package { ["python-pip",
